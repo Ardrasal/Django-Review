a3baa759dec43eb2a3b7ed2af97badaf93b7a97e
Ardra Salemy <ardrasalemy@gmail.com>
2019-02-09 23:29:33 -0500

    Posts and details page render, but not showing body and created_at on details page.

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,14 @@ class Posts(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+    """
+    Changes name of Post in admin from 'object post 1' to title of post.
+    """
+    def __str__(self):
+        return self.title
+        
+    class Meta:
+        """
+        Changes Postss in admin to Posts.
+        """
+        verbose_name_plural = "Posts"
